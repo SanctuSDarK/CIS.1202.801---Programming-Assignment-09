fb58698cfd2f143eac51af6328eebbd883c99f86
david <david@RAEDWULF-PC>
2024-03-01 18:56:22 -0500

    Add project files.

---
+++ CIS.1202.801 - Programming Assignment 09.cpp
@@ -0,0 +1,21 @@
+/* David Belvin
+CIS.1202.800
+Febuary 4th, 2024
+Professor Bowman
+
+CIS 1202 Programming Assignment 09
+Source Code Management 1
+(Summer 2023 version)
+
+Write a program that displays the text "Hello, World", 
+check it into a source code management system, modify 
+the program, and check in the update. */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
